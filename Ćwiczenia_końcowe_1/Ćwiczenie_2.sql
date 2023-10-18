69c3b78ca0b9fe39506949c7c327389c6d9b96d1
Konrad <karmatys888@gmail.com>
2023-10-18 12:10:31 +0200

    Upadted repo structure
    Additionally fixed an error in previous soultion.

--- Ćwiczenia_końcowe_1/Ćwiczenie_2.sql
+++ Ćwiczenia_końcowe_1/Ćwiczenie_2.sql
@@ -1,33 +1,23 @@
-/*
 --1.
 SELECT title
 FROM title
 ORDER BY title
-*/
 
-/*
 --2.
 SELECT member_no, isbn, fine_assessed, fine_assessed * 2 as "double fine"
 FROM loanhist
 WHERE fine_assessed != 0 AND fine_assessed IS NOT NULL
-*/
 
-/*
 --3.2.
 SELECT firstname + middleinitial + lastname AS email_name
 FROM member
 WHERE lastname = 'Anderson'
-*/
 
-/*
 --3.3.
 SELECT LOWER(firstname + middleinitial + SUBSTRING(lastname, 1, 2))
 AS "listê proponowanych loginów e-mail"
 FROM member
-*/
 
-/*
 --4.
 SELECT 'The title is: ' + title + ', title number ' + CAST(title_no as nchar)
 FROM title
-*/
