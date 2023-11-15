78443ffece29e74e3a03c0dbd4dfa572ea44903c
Konrad <karmatys888@gmail.com>
2023-11-15 14:54:54 +0100

    Corrected task
    During checking task I have realised some mistakes that I have made in some of my solutions.

--- Ćwiczenia_końcowe_3/Ćwiczenie_4.sql
+++ Ćwiczenia_końcowe_3/Ćwiczenie_4.sql
@@ -1,18 +1,22 @@
 --1a.
-SELECT Employees.Firstname, Employees.Lastname, COUNT(OrderID) AS 'Total orders'
+SELECT DISTINCT Employees.Firstname, Employees.Lastname, ROUND(SUM(Quantity*UnitPrice*(1 - Discount)), 2) AS 'Total price'
 FROM Employees
 INNER JOIN Orders
 ON Orders.EmployeeID = Employees.EmployeeID
-INNER JOIN Employees as e
-ON e.ReportsTo = Employees.EmployeeID
-GROUP BY Employees.Employeeid, Employees.Lastname, Employees.Firstname
+INNER JOIN Employees as subordinate
+ON subordinate.ReportsTo = Employees.EmployeeID
+INNER JOIN [Order Details]
+ON Orders.OrderID = [Order Details].OrderID
+GROUP BY Employees.Lastname, Employees.Firstname, Employees.Employeeid, subordinate.EmployeeID
 
 --1b.
-SELECT Employees.Firstname, Employees.Lastname, COUNT(OrderID) AS 'Total orders'
+SELECT Employees.Firstname, Employees.Lastname, ROUND(SUM(Quantity*UnitPrice*(1 - Discount)), 2) AS 'Total price'
 FROM Employees
 INNER JOIN Orders
 ON Orders.EmployeeID = Employees.EmployeeID
 LEFT OUTER JOIN Employees as e
 ON e.ReportsTo = Employees.EmployeeID
+INNER JOIN [Order Details]
+ON Orders.OrderID = [Order Details].OrderID
 WHERE e.ReportsTo is NULL
 GROUP BY Employees.Employeeid, Employees.Lastname, Employees.Firstname
